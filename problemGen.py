d09321223445edfcfb5fa70a55ab32637af3b29a
Jan <2002jan@gmail.com>
2022-06-04 12:19:16 +0200

    added problem generator

---
+++ problemGen.py
@@ -0,0 +1,38 @@
+import random, os
+
+def appendweight(weights, targetLen):
+    while len(weights) != targetLen:
+        weights.append(random.randrange(1, 100))
+    return weights
+
+def appendvalue(values, targetLen):
+    while len(values) != targetLen:
+        values.append(random.randrange(1, 100))
+    return values
+
+numofitems = [x for x in range(20, 620, 20)]
+
+if not os.path.isdir("problem_instances"):
+    os.mkdir("problem_instances")
+
+for i in numofitems:
+    for x in range(10):
+        capacity = random.randrange(50, i*4)
+
+        weights = []
+        appendweight(weights, i)
+
+        while sum(weights) <= capacity:
+            weights = appendweight(weights, i)
+
+        values = []
+        appendvalue(values, i)
+        filename = f"problem_instances/problem_{i}_{x + 1}.txt"
+
+        with open(filename, "w") as f:
+
+            f.write(str(capacity))
+            f.write("\n")
+
+            for w, v in zip(weights, values):
+                f.write(f"{w} {v}\n")
